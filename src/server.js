5f1b2a8effde1d96dda8f30cf61af378c3083d18
igor-ribeir0 <igor_ribeirosantos@outlook.com>
2023-01-31 18:02:55 -0300

    feat: GET/poll, POST/poll, POST/choice

---
+++ src/server.js
@@ -0,0 +1,104 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import joi from "joi";
+import dayjs from "dayjs";
+import { MongoClient, ObjectId } from "mongodb";
+
+dotenv.config();
+
+const server = express();
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try{
+    await mongoClient.connect();
+    db = mongoClient.db();
+}
+catch(error){
+    console.log("Connection Server Error");
+}
+
+server.use(cors());
+server.use(express.json());
+server.listen(process.env.PORT);
+
+server.get("/poll", async(req, res) => {
+    try{
+        const getData = await db.collection("poll").find().toArray();
+        return res.status(200).send(getData);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
+
+server.post("/poll", async(req, res) => {
+    const { title, expireAt } = req.body;
+    const schema = joi.object({ title: joi.string().required() });
+    const poll = { title };
+    const validation = schema.validate(poll);
+    let expire;
+
+    if(validation.error){
+        return res.status(422).send("Title não pode ser uma String vazia.");
+    };
+
+    if(expireAt.length === 0){
+        expire = `30 days ${dayjs().format("HH:mm")}`;
+    }
+    else{
+        expire = expireAt;
+    }
+
+    try{
+        await db.collection("poll").insertOne({ title, expire });
+        return res.sendStatus(201);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+
+});
+
+server.post("/choice", async(req, res) => {
+    const { title, pollId } = req.body;
+    const schema = joi.object(
+        {
+            title: joi.string().required(),
+            pollId: joi.string().required()
+        }
+    );
+    const choice = { title, pollId};
+    const validation = schema.validate(choice, { abortEarly: false });
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try{
+        const searchId = await db.collection("poll").findOne({ _id: ObjectId(pollId) });
+        const searchChoiceTitle = await db.collection("choice").findOne({ title });
+
+        if(!searchId){
+            return res.status(404).send("Uma opção de voto não pode ser inserida sem uma enquete existente");
+        }
+
+        if(searchChoiceTitle){
+            return res.status(409).send("Title já existente");
+        }
+
+        await db.collection("choice").insertOne(
+            {
+                title,
+                pollId
+            }
+        );
+
+        return res.sendStatus(201);
+    }
+    catch(error){
+        return res.status(500).send(error.message);
+    }
+});
